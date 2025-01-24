4d7bb735896410f7ddf36ab716ad957c823a4886
Jerremy Asuncion <asuncionjerremyph@gmail.com>
2025-01-24 22:40:15 +0800

    Set up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <main>
+            <h1>RTK QUERY</h1>
+            <button>Get Message from Backend</button>
+        </main>
+    );
+}
+
+export default App;
